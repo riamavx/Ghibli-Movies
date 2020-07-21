12c72cc4264ff24632007ea6626c6aca247ee28f
Maria Souza <messenger.mariah2017@gmail.com>
2020-07-21 11:14:15 -0300

    responsivo mobile first home

--- javascript/script.js
+++ javascript/script.js
@@ -1,8 +1,8 @@
 
   $('.filmes__destaque').slick({
     infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 3,
+    slidesToShow: 1,
+    slidesToScroll: 1,
     prevArrow: $("#galeria-prev"),
     nextArrow: $("#galeria-next"),
   });
